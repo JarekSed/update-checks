33f8ab32a60f507519f16b9246b81bd94bbf632a
Jarek Sedlacek <jarek@jsedlacek.info>
2013-03-20 14:40:45 -0400

    Better program module setup
    renamed scrape to php-mongo
    updatechecks.programs.__init__ now can get all program modules, and can
    do checking for all of them

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='updatechecks',
-      version='0.2',
+      version='0.3',
       description='Checks web pages to determine if programs have updates',
       url='http://github.com/JarekSed/update-check',
       author='Jarek Sedlacek',
---
+++ updatechecks/programs/__init__.py
@@ -0,0 +1,28 @@
+""" The Programs submodule is used to have custom
+    scripts to scrape and parse the website for each program."""
+
+import os
+import pkgutil
+from pkg_resources import parse_version
+
+def get_all_modules():
+    """ Lists all modules in this package. This is used so we can just drop
+    another module in here for some program, and it will automatically be
+    picked up."""
+    pkgpath = os.path.dirname(__file__)
+    programs = [name for _, name, _ in pkgutil.iter_modules([pkgpath])]
+    return programs
+
+
+
+def check_all_programs():
+    modules = get_all_modules()
+    for module_name in modules:
+        module = __import__("updatechecks.programs."+module_name,globals(),locals(), ['a'], -1)
+        print "impmorted" + module_name
+        print module
+        current_version = module.get_version()
+        if parse_version(current_version) > parse_version(module.get_last_known_version()):
+            print module_name, "is out of date, newest version is", current_version
+        else:
+            print module_name, "looks good, current version is", current_version
