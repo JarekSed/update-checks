fe266fa4006c936235a5d1584d65d7064eb72421
Jarek Sedlacek <jarek@jsedlacek.info>
2013-06-04 15:30:15 -0400

    added email notification library (and call in executable)

---
+++ go/src/notify/Email.go
@@ -0,0 +1,28 @@
+package notify
+
+import (
+        "bytes"
+        "net/smtp"
+)
+
+func SendMail(to_addr string, from_addr string, host string, subject string, body string) (error) {
+        // Connect to the remote SMTP server.
+        c, err := smtp.Dial(host)
+        if err != nil {
+                return err
+        }
+        // Set the sender and recipient.
+        c.Mail(from_addr)
+        c.Rcpt(to_addr)
+        // Send the email body.
+        wc, err := c.Data()
+        if err != nil {
+            return err
+        }
+        defer wc.Close()
+        buf := bytes.NewBufferString("Subject: " + subject + "\r\n" + "To: " + to_addr + "\r\n\r\n" + body)
+        if _, err = buf.WriteTo(wc); err != nil {
+            return err
+        }
+        return nil
+}
--- go/src/updatechecks.go
+++ go/src/updatechecks.go
@@ -3,11 +3,26 @@ package main
 import (
     "checks"
     "fmt"
+    "flag"
+    "os"
+    "notify"
 )
+
+var toAddr = flag.String("to-address", "jareksedlacek@gmail.com", "Email address to send to")
+var fromAddr = flag.String("from-address", "updatechecks@jsedlacek.info", "Email address to send from")
+var host = flag.String("email-host", "localhost", "Host to send email from")
+var port = flag.String("email-port", "25", "Port to connect to host on")
 
 func main() {
     outOfDatePrograms := checks.GetOutOfDatePrograms()
-    for _, program := range outOfDatePrograms{
-        fmt.Printf("%v\n",program)
+    message := notify.BuildOutOfDateMessage(outOfDatePrograms)
+    if message != "" {
+        fmt.Printf("%v", message)
+        err := notify.SendMail(*toAddr, *fromAddr, *host + ":" + *port, "Program updates found!", message)
+        if err != nil {
+            fmt.Fprintf(os.Stdout, "Error sending mail: %v\n", err)
+        }
+    } else {
+        fmt.Printf("No updates found\n")
     }
 }
