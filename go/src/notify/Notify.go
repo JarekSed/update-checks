d1ebcb844884e36bb60330c353f0fe525b3525a2
Jarek Sedlacek <jarek@jsedlacek.info>
2013-06-05 20:33:25 -0400

    ran through go fmt

--- go/src/notify/Notify.go
+++ go/src/notify/Notify.go
@@ -1,17 +1,17 @@
 package notify
 
 import (
-    "bytes"
-    "checks"
+	"bytes"
+	"checks"
 )
 
-func BuildOutOfDateMessage( outOfDatePrograms []checks.OutOfDateProgram) (string) {
-    var message bytes.Buffer
-    for _, program := range outOfDatePrograms {
-        message.WriteString(program.Name + " appears to have a new release! Version " + program.LatestVersion + " upstream is newer than current AUR version "+ program.AurVersion +"  " )
-        message.WriteString("https://aur.archlinux.org/packages/" + program.Name + "\n")
-    }
+func BuildOutOfDateMessage(outOfDatePrograms []checks.OutOfDateProgram) string {
+	var message bytes.Buffer
+	for _, program := range outOfDatePrograms {
+		message.WriteString(program.Name + " appears to have a new release! Version " + program.LatestVersion + " upstream is newer than current AUR version " + program.AurVersion + "  ")
+		message.WriteString("https://aur.archlinux.org/packages/" + program.Name + "\n")
+	}
 
-    return message.String()
+	return message.String()
 
 }
