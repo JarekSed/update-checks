fcfc2ae57dbb74f8ed4ce05ba167f7b573f60ba8
Jarek Sedlacek <jarek@jsedlacek.info>
2013-06-05 20:44:04 -0400

    Added quiet flag, and bugfix
    Remembered to actually parse the flags before trying to use them.

--- go/src/updatechecks/updatechecks.go
+++ go/src/updatechecks/updatechecks.go
@@ -12,15 +12,19 @@ var toAddr = flag.String("to-address", "jareksedlacek@gmail.com", "Email address
 var fromAddr = flag.String("from-address", "updatechecks@jsedlacek.info", "Email address to send from")
 var host = flag.String("email-host", "localhost", "Host to send email from")
 var port = flag.String("email-port", "25", "Port to connect to host on")
+var quiet = flag.Bool("quiet", false, "Only print to stdout, don't send email")
 
 func main() {
+	flag.Parse()
 	outOfDatePrograms := checks.GetOutOfDatePrograms()
 	message := notify.BuildOutOfDateMessage(outOfDatePrograms)
 	if message != "" {
 		fmt.Printf("%v", message)
-		err := notify.SendMail(*toAddr, *fromAddr, *host+":"+*port, "Program updates found!", message)
-		if err != nil {
-			fmt.Fprintf(os.Stdout, "Error sending mail: %v\n", err)
+		if !*quiet {
+			err := notify.SendMail(*toAddr, *fromAddr, *host+":"+*port, "Program updates found!", message)
+			if err != nil {
+				fmt.Fprintf(os.Stdout, "Error sending mail: %v\n", err)
+			}
 		}
 	} else {
 		fmt.Printf("No updates found\n")
